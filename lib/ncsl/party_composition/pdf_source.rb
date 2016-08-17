a5b2e4317ae8719c4c33fbe896966698a52c7a98
MJ Rossetti <mrossetti@aee.net>
2016-08-17 14:13:45 -0400

    Handle unicameral legislatures

--- lib/ncsl/party_composition/pdf_source.rb
+++ lib/ncsl/party_composition/pdf_source.rb
@@ -28,7 +28,9 @@ module Ncsl
 
       DATA_DIR = File.expand_path("../../../../data/party_composition", __FILE__)
       CELL_DELIMETER = "   "
+      BLANK_CELL_REPLACEMENT_VALUE = "#{CELL_DELIMETER}0#{CELL_DELIMETER}"
       DOUBLE_CELL_DELIMETER = "                   "
+      DOUBLE_CELL_DELIMETER_UNICAMERAL = "            "
 
       def self.all
         Ncsl::PartyComposition::PDF_SOURCES.map{|source| new(source) }
@@ -96,31 +98,44 @@ module Ncsl
         lines.reject!{|line| line.include?("Territories") }
         lines.reject!{|line| line.include?("Abbreviations") }
         lines.reject!{|line| line.include?("Notes:") }
-
-        # get rid of all lines between wyoming and american samoa
+        # remove line(s) between wyoming and american samoa ...
         wyoming_line = lines.find{|line| line.include?("Wyoming") }
         samoa_line = lines.find{|line| line.include?("American Samoa") }
         indices_of_lines_to_remove = (lines.index(wyoming_line) + 1 .. lines.index(samoa_line) - 1).to_a
         number_of_lines_to_remove = indices_of_lines_to_remove.count
         lines_to_remove = lines.slice(indices_of_lines_to_remove.first, number_of_lines_to_remove)
         lines -= lines_to_remove
+
         raise LineCountError.new(lines.count) unless lines.count == 56
         return lines
       end
 
-      def parsed_line(line)
-        binding.pry if line.include?("Unicameral")
+      def parse_line(line)
+        parsed_line = line
+        parsed_line.gsub!(" e ", CELL_DELIMETER) # workaround for erroneous letter in 2013/2014: Florida, Georgia, Idaho, Indiana, Kansas, Louisiana, Michigan, Wisconsin ...
 
-        line.gsub!(" e ", CELL_DELIMETER) # workaround for 2013/2014: Florida, Georgia, Idaho, Indiana, Kansas, Louisiana, Michigan, Wisconsin ...
+        if parsed_line.include?("Unicameral") # workaround for unicameral legislature blank values ...
+          parsed_line.gsub!( parsed_line.slice(0,24) , "#{parsed_line.slice(0,24).strip}#{CELL_DELIMETER}")
 
-        if [2015,2016].include?(year)
-          line.gsub!( line.slice(0,25) , "#{line.slice(0,25).strip}#{CELL_DELIMETER}")
-          line.gsub!(DOUBLE_CELL_DELIMETER, "#{CELL_DELIMETER}0#{CELL_DELIMETER}")
-        end # workaround for 2015 and 2016 where blank values should be zeros ...
+          #binding.pry if year == 2016 && (parsed_line.include?("District of Columbia") || parsed_line.include?("Guam") )
 
-        line.gsub!("Rep Rep Dem", "Rep#{CELL_DELIMETER}Dem") if year == 2016 && line.include?("Louisiana") # workaround for 2016 Louisiana where "Rep Rep Dem" should be "Rep    Dem" ...
 
-        return line
+          if [2015,2016].include?(year) && (parsed_line.include?("District of Columbia") || parsed_line.include?("Guam") )
+            parsed_line.gsub!("      Unicameral","UNI")
+            parsed_line.gsub!(DOUBLE_CELL_DELIMETER_UNICAMERAL, BLANK_CELL_REPLACEMENT_VALUE )
+            parsed_line.gsub!("UNI","Unicameral")
+          else
+            parsed_line.gsub!(DOUBLE_CELL_DELIMETER_UNICAMERAL, BLANK_CELL_REPLACEMENT_VALUE)
+          end
+          parsed_line.gsub!(parsed_line[-15,15], parsed_line[-15,15].gsub("0","")) # remove erroneously-added 0 between last two control values if necessary (for huge space in District of Columbia lines)
+        elsif [2015,2016].include?(year) # workaround for 2015 and 2016 where blank values should be zeros ...
+          parsed_line.gsub!( parsed_line.slice(0,25) , "#{parsed_line.slice(0,25).strip}#{CELL_DELIMETER}")
+          parsed_line.gsub!(DOUBLE_CELL_DELIMETER, BLANK_CELL_REPLACEMENT_VALUE)
+        end
+
+        parsed_line.gsub!("Rep Rep Dem", "Rep#{CELL_DELIMETER}Dem") if year == 2016 && parsed_line.include?("Louisiana") # workaround for 2016 Louisiana where "Rep Rep Dem" should be "Rep    Dem" ...
+
+        return parsed_line
       end
 
       def convert_txt_to_csv
@@ -130,13 +145,13 @@ module Ncsl
           txt_lines.each do |txt_line|
             begin
               next if txt_line.include?("Non-partisan")
-              next if txt_line.include?("Unicam")
-              line = parsed_line(txt_line)
+              line = parse_line(txt_line)
               cells = line.split(CELL_DELIMETER).map{|l| l.strip } - [""]
               puts "... #{cells.first}"
               cells = cells.insert(-2, "NULL") if year == 2015 && ["Mariana Islands"].include?(cells.first) # workaround for null gov_party values
-              cells[11].gsub!("0", "NULL") if year == 2016 && ["Mariana Islands"].include?(cells.first) # workaround for null gov_party values
+              cells[11].gsub!("0", "NULL") #if year == 2016 && ["Mariana Islands"].include?(cells.first) # workaround for null gov_party values
               raise CellCountError unless cells.count == 13
+              #TODO: add boolean column for unicameral, add proper values, replace "Unicameral" with 0, and raise CellCountError unless cells.count == 14
               raise LegislatureControlError unless LEGISLATURE_CONTROL_VALUES.include?(cells[10].gsub(CONTROL_BY_COALITION_MARKER,""))
               raise GovernorPartyError unless GOVERNOR_PARTY_VALUES.include?(cells[11])
               raise StateControlError unless STATE_CONTROL_VALUES.include?(cells[12].gsub(CONTROL_BY_COALITION_MARKER,""))
